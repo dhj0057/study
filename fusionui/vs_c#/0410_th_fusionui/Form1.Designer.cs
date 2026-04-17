e5826c3e3a404e54e5208d160f74c3ee7d10d88a
hyeong jun <dhj00570@gmail.com>
2026-04-17 11:51:10 +0900

    fusionUI

--- fusionui/vs_c#/0410_th_fusionui/Form1.Designer.cs
+++ fusionui/vs_c#/0410_th_fusionui/Form1.Designer.cs
@@ -32,11 +32,12 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // myButton
             // 
-            this.myButton.Location = new System.Drawing.Point(342, 149);
+            this.myButton.Location = new System.Drawing.Point(232, 170);
             this.myButton.Name = "myButton";
             this.myButton.Size = new System.Drawing.Size(153, 103);
             this.myButton.TabIndex = 0;
@@ -70,11 +71,21 @@
             this.label1.TabIndex = 3;
             this.label1.Text = "label1";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(705, 149);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(836, 529);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -93,6 +104,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
---
+++ fusionui/vs_c#/fusionui.0417/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fusionui._0417
+{
+    public partial class form1 : Form
+    {
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            result.Text += "1";   
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            result.Text += "2";
+        }
+       
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            result.Text += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            result.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            result.Text += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            result.Text += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            result.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            result.Text += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            result.Text += "9";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            result.Text += "0";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            result.Text  = string.Empty;
+        }
+        int firstNUM;
+        int secondNUM;
+        int resultNUM;
+        string Relation; 
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+           
+            firstNUM = int.Parse(result.Text);
+
+            Relation = "+";
+            
+            result.Text = string.Empty;
+       
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)(sender);
+          
+            secondNUM = int.Parse(result.Text);
+
+            if (Relation == "+")
+            {
+                resultNUM = firstNUM + secondNUM;
+            }
+            else
+            {
+                resultNUM = firstNUM - secondNUM;
+            }
+
+            result.Text = resultNUM.ToString();
+
+
+        }
+      
+        private void button14_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            firstNUM = int.Parse(result.Text);
+
+            Relation = "-";
+
+            result.Text = string.Empty;
+
+        }
+    }
+}
