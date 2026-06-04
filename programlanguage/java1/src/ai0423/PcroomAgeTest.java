b4e772113198fc7e3204e78d26056701f867b5ce
hyeong jun <dhj00570@gmail.com>
2026-06-04 11:46:16 +0900

    수업저장

--- programlanguage/java1/src/ai0423/PcroomAgeTest.java
+++ programlanguage/java1/src/ai0423/PcroomAgeTest.java
@@ -1,22 +1,24 @@
+package ai0423;
 import java.util.Scanner;
 
-public static void main(String[] args) {
-    Scanner s = new Scanner(System.in);
+public class PcroomAgeTest {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
 
-    System.out.print("나이를 입력하세요: ");
+        System.out.print("나이를 입력하세요: ");
 
-    int age = s.nextInt();
-    String result = "";
+        int age = s.nextInt();
+        String result = "";
 
-    if (age >= 19) {
-        result = "가능";
-    } else {
-        result = "불가능";
+        if (age >= 19) {
+            result = "가능";
+        } else {
+            result = "불가능";
+        }
+
+        System.out.printf("당신의 나이는 %d세이므로 오후 10시 이후 PC방 사용이 %s합니다.\n", age, result);
+        System.out.println("협조해 주셔서 감사합니다.");
+
+        s.close();
     }
-
-    System.out.printf("당신의 나이는 %d세이므로 오후 10시 이후 PC방 사용이 %s합니다.\n", age, result);
-    System.out.println("협조해 주셔서 감사합니다.");
-
-    s.close();
 }
-
