6da3207d110726746a5c7a1f7c42637253845b52
hyeong jun <dhj00570@gmail.com>
2026-06-11 10:14:48 +0900

    수업

---
+++ programlanguage/java1/src/ai0611/lab/subCar/SportsCar.java
@@ -0,0 +1,12 @@
+package ai0611.lab.subCar;
+
+import  ai0611.lab.car.Car;
+public class SportsCar extends Car {
+    @Override
+    public void upSpeed(int speed){
+        super.upSpeed(speed);
+        if(this.speed >= 200){
+            this.speed = 200;
+        }
+    }
+}
