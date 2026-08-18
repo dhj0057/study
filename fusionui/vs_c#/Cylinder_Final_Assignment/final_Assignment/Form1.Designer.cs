4123a82c673c58bee6200aa282dacf50c928971f
hyeong jun <dhj00570@gmail.com>
2026-08-18 14:05:06 +0900

    2학기 자ë바 수업

---
+++ fusionui/vs_c#/Cylinder_Final_Assignment/final_Assignment/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace final_Assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonAutoStart = new System.Windows.Forms.Button();
+            this.buttonAutoStop = new System.Windows.Forms.Button();
+            this.buttonBForward = new System.Windows.Forms.Button();
+            this.buttonBBack = new System.Windows.Forms.Button();
+            this.buttonCForward = new System.Windows.Forms.Button();
+            this.buttonCBack = new System.Windows.Forms.Button();
+            this.buttonLiftAUp = new System.Windows.Forms.Button();
+            this.buttonLiftADown = new System.Windows.Forms.Button();
+            this.buttonLiftBUp = new System.Windows.Forms.Button();
+            this.buttonLiftBDown = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.labelSensor = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(12, 12);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(100, 50);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "연결";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonAutoStart
+            // 
+            this.buttonAutoStart.Location = new System.Drawing.Point(118, 12);
+            this.buttonAutoStart.Name = "buttonAutoStart";
+            this.buttonAutoStart.Size = new System.Drawing.Size(100, 50);
+            this.buttonAutoStart.TabIndex = 1;
+            this.buttonAutoStart.Text = "자동 시작";
+            this.buttonAutoStart.UseVisualStyleBackColor = true;
+            this.buttonAutoStart.Click += new System.EventHandler(this.buttonAutoStart_Click);
+            // 
+            // buttonAutoStop
+            // 
+            this.buttonAutoStop.Location = new System.Drawing.Point(224, 12);
+            this.buttonAutoStop.Name = "buttonAutoStop";
+            this.buttonAutoStop.Size = new System.Drawing.Size(100, 50);
+            this.buttonAutoStop.TabIndex = 2;
+            this.buttonAutoStop.Text = "자동 정지";
+            this.buttonAutoStop.UseVisualStyleBackColor = true;
+            this.buttonAutoStop.Click += new System.EventHandler(this.buttonAutoStop_Click);
+            // 
+            // buttonBForward
+            // 
+            this.buttonBForward.Location = new System.Drawing.Point(12, 68);
+            this.buttonBForward.Name = "buttonBForward";
+            this.buttonBForward.Size = new System.Drawing.Size(100, 50);
+            this.buttonBForward.TabIndex = 3;
+            this.buttonBForward.Text = "B실린더 전진";
+            this.buttonBForward.UseVisualStyleBackColor = true;
+            this.buttonBForward.Click += new System.EventHandler(this.buttonBForward_Click);
+            // 
+            // buttonBBack
+            // 
+            this.buttonBBack.Location = new System.Drawing.Point(118, 68);
+            this.buttonBBack.Name = "buttonBBack";
+            this.buttonBBack.Size = new System.Drawing.Size(100, 50);
+            this.buttonBBack.TabIndex = 4;
+            this.buttonBBack.Text = "B실린더 후진";
+            this.buttonBBack.UseVisualStyleBackColor = true;
+            this.buttonBBack.Click += new System.EventHandler(this.buttonBBack_Click);
+            // 
+            // buttonCForward
+            // 
+            this.buttonCForward.Location = new System.Drawing.Point(12, 126);
+            this.buttonCForward.Name = "buttonCForward";
+            this.buttonCForward.Size = new System.Drawing.Size(100, 50);
+            this.buttonCForward.TabIndex = 5;
+            this.buttonCForward.Text = "C실린더 전진";
+            this.buttonCForward.UseVisualStyleBackColor = true;
+            this.buttonCForward.Click += new System.EventHandler(this.buttonCForward_Click);
+            // 
+            // buttonCBack
+            // 
+            this.buttonCBack.Location = new System.Drawing.Point(118, 124);
+            this.buttonCBack.Name = "buttonCBack";
+            this.buttonCBack.Size = new System.Drawing.Size(100, 50);
+            this.buttonCBack.TabIndex = 6;
+            this.buttonCBack.Text = "C실린더 후진";
+            this.buttonCBack.UseVisualStyleBackColor = true;
+            this.buttonCBack.Click += new System.EventHandler(this.buttonCBack_Click);
+            // 
+            // buttonLiftAUp
+            // 
+            this.buttonLiftAUp.Location = new System.Drawing.Point(12, 180);
+            this.buttonLiftAUp.Name = "buttonLiftAUp";
+            this.buttonLiftAUp.Size = new System.Drawing.Size(100, 50);
+            this.buttonLiftAUp.TabIndex = 7;
+            this.buttonLiftAUp.Text = "LiftA Up";
+            this.buttonLiftAUp.UseVisualStyleBackColor = true;
+            this.buttonLiftAUp.Click += new System.EventHandler(this.buttonLiftAUp_Click);
+            // 
+            // buttonLiftADown
+            // 
+            this.buttonLiftADown.Location = new System.Drawing.Point(118, 180);
+            this.buttonLiftADown.Name = "buttonLiftADown";
+            this.buttonLiftADown.Size = new System.Drawing.Size(100, 50);
+            this.buttonLiftADown.TabIndex = 8;
+            this.buttonLiftADown.Text = "LiftA Down";
+            this.buttonLiftADown.UseVisualStyleBackColor = true;
+            this.buttonLiftADown.Click += new System.EventHandler(this.buttonLiftADown_Click);
+            // 
+            // buttonLiftBUp
+            // 
+            this.buttonLiftBUp.Location = new System.Drawing.Point(12, 236);
+            this.buttonLiftBUp.Name = "buttonLiftBUp";
+            this.buttonLiftBUp.Size = new System.Drawing.Size(100, 50);
+            this.buttonLiftBUp.TabIndex = 9;
+            this.buttonLiftBUp.Text = "LiftB Up";
+            this.buttonLiftBUp.UseVisualStyleBackColor = true;
+            this.buttonLiftBUp.Click += new System.EventHandler(this.buttonLiftBUp_Click);
+            // 
+            // buttonLiftBDown
+            // 
+            this.buttonLiftBDown.Location = new System.Drawing.Point(118, 236);
+            this.buttonLiftBDown.Name = "buttonLiftBDown";
+            this.buttonLiftBDown.Size = new System.Drawing.Size(100, 50);
+            this.buttonLiftBDown.TabIndex = 10;
+            this.buttonLiftBDown.Text = "LiftB Down";
+            this.buttonLiftBDown.UseVisualStyleBackColor = true;
+            this.buttonLiftBDown.Click += new System.EventHandler(this.buttonLiftBDown_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.labelStatus.Location = new System.Drawing.Point(257, 79);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(143, 21);
+            this.labelStatus.TabIndex = 12;
+            this.labelStatus.Text = "현재 상태표시";
+            // 
+            // labelSensor
+            // 
+            this.labelSensor.AutoSize = true;
+            this.labelSensor.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.labelSensor.Location = new System.Drawing.Point(257, 139);
+            this.labelSensor.Name = "labelSensor";
+            this.labelSensor.Size = new System.Drawing.Size(135, 19);
+            this.labelSensor.TabIndex = 13;
+            this.labelSensor.Text = "센서 상태 대기";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelSensor);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonLiftBDown);
+            this.Controls.Add(this.buttonLiftBUp);
+            this.Controls.Add(this.buttonLiftADown);
+            this.Controls.Add(this.buttonLiftAUp);
+            this.Controls.Add(this.buttonCBack);
+            this.Controls.Add(this.buttonCForward);
+            this.Controls.Add(this.buttonBBack);
+            this.Controls.Add(this.buttonBForward);
+            this.Controls.Add(this.buttonAutoStop);
+            this.Controls.Add(this.buttonAutoStart);
+            this.Controls.Add(this.buttonConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonAutoStart;
+        private System.Windows.Forms.Button buttonAutoStop;
+        private System.Windows.Forms.Button buttonBForward;
+        private System.Windows.Forms.Button buttonBBack;
+        private System.Windows.Forms.Button buttonCForward;
+        private System.Windows.Forms.Button buttonCBack;
+        private System.Windows.Forms.Button buttonLiftAUp;
+        private System.Windows.Forms.Button buttonLiftADown;
+        private System.Windows.Forms.Button buttonLiftBUp;
+        private System.Windows.Forms.Button buttonLiftBDown;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Label labelSensor;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ fusionui/vs_c#/Cylinder_Final_Assignment/final_Assignment/Form1.cs
@@ -0,0 +1,439 @@
+﻿using ACTMULTILIB_K;
+using System;
+using System.Windows.Forms;
+
+namespace final_Assignment
+{
+    public partial class Form1 : Form
+    {
+
+        private readonly ActEasyIF control = new ActEasyIF();
+
+        private bool autoMode = false;
+        private bool isConnected = false;
+        private int autoStep = 0;
+
+        private const int LOGICAL_STATION_NUMBER = 6;
+        private const string X_DEVICE = "X0";
+        private const string Y_DEVICE = "Y0";
+
+        // X 입력 센서
+        private const short X_B_FORWARD = 0x0004;     // X02: B실린더 전진
+        private const short X_B_BACK = 0x0008;        // X03: B실린더 후진
+        private const short X_C_BACK = 0x0010;        // X04: C실린더 후진
+        private const short X_C_FORWARD = 0x0020;     // X05: C실린더 전진
+
+        private const short X_LIFTA_UP = 0x0040;      // X6: LiftA Up
+        private const short X_LIFTA_DOWN = 0x0080;    // X7: LiftA Down
+        private const short X_LIFTB_UP = 0x0100;      // X8: LiftB Up
+        private const short X_LIFTB_DOWN = 0x0200;    // X9: LiftB Down
+
+        private const short X_SENSOR_A = 0x0400;      // XA: 리프트센서A
+        private const short X_SENSOR_B = 0x0800;      // XB: 리프트센서B
+
+        // Y 출력 장치
+        private const short Y_B_FORWARD = 0x0002;     // Y01: B실린더 전진
+        private const short Y_B_BACK = 0x0004;        // Y02: B실린더 후진
+        private const short Y_C_FORWARD = 0x0008;     // Y03: C실린더 전진
+        private const short Y_C_BACK = 0x0010;        // Y04: C실린더 후진
+
+        private const short Y_LIFTA_UP = 0x0020;      // Y5: LiftA Up
+        private const short Y_LIFTA_DOWN = 0x0040;    // Y6: LiftA Down
+        private const short Y_LIFTB_DOWN = 0x0080;    // Y7: LiftB Down
+        private const short Y_LIFTB_UP = 0x0100;      // Y8: LiftB Up
+
+
+        public Form1()
+
+        {
+            InitializeComponent();
+
+            timer1.Enabled = false;
+            timer1.Interval = 500;
+        }
+        private bool IsOn(short sensor, short bit)
+        {
+            return (sensor & bit) != 0;
+        }
+
+        private string OnOff(bool value)
+        {
+            return value ? "ON" : "OFF";
+        }
+
+        private bool WriteY(short value)
+        {
+            if (!isConnected)
+            {
+                labelStatus.Text = "현재 상태: PLC 미연결";
+                return false;
+            }
+
+            try
+            {
+                int result = control.WriteDeviceBlock2(Y_DEVICE, 1, ref value);
+
+                if (result != 0)
+                {
+                    labelStatus.Text = "현재 상태: 출력 실패 코드 " + result;
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                timer1.Enabled = false;
+                isConnected = false;
+                autoMode = false;
+
+                MessageBox.Show("출력 오류: " + ex.Message);
+                labelStatus.Text = "현재 상태: PLC 출력 예외 발생";
+                return false;
+            }
+        }
+
+        private bool ReadSensor(out short sensor)
+        {
+            sensor = 0;
+
+            if (!isConnected)
+            {
+                return false;
+            }
+
+            try
+            {
+                int result = control.ReadDeviceBlock2(X_DEVICE, 1, out sensor);
+
+                if (result != 0)
+                {
+                    labelStatus.Text = "현재 상태: 센서 읽기 실패 코드 " + result;
+                    return false;
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                timer1.Enabled = false;
+                isConnected = false;
+                autoMode = false;
+
+                MessageBox.Show("센서 읽기 오류: " + ex.Message);
+                labelStatus.Text = "현재 상태: 센서 읽기 예외 발생";
+                return false;
+            }
+        }
+
+        
+        
+        private void UpdateSensorLabel(short sensor)
+        {
+            labelSensor.Text =
+                "X0 전체값: " + sensor + "\n\n" +
+                "B전진 X02: " + OnOff(IsOn(sensor, X_B_FORWARD)) + "\n" +
+                "B후진 X03: " + OnOff(IsOn(sensor, X_B_BACK)) + "\n" +
+                "C전진 X05: " + OnOff(IsOn(sensor, X_C_FORWARD)) + "\n" +
+                "C후진 X04: " + OnOff(IsOn(sensor, X_C_BACK)) + "\n" +
+                "LiftA Up X6: " + OnOff(IsOn(sensor, X_LIFTA_UP)) + "\n" +
+                "LiftA Down X7: " + OnOff(IsOn(sensor, X_LIFTA_DOWN)) + "\n" +
+                "LiftB Up X8: " + OnOff(IsOn(sensor, X_LIFTB_UP)) + "\n" +
+                "LiftB Down X9: " + OnOff(IsOn(sensor, X_LIFTB_DOWN)) + "\n" +
+                "센서A XA: " + OnOff(IsOn(sensor, X_SENSOR_A)) + "\n" +
+                "센서B XB: " + OnOff(IsOn(sensor, X_SENSOR_B));
+        }
+        private void ManualOutput(short outputValue, string statusText)
+        {
+            if (WriteY(outputValue))
+            {
+                labelStatus.Text = "현재 상태: " + statusText;
+            }
+        }
+
+
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                control.ActLogicalStationNumber = LOGICAL_STATION_NUMBER;
+
+                int openResult = control.Open();
+
+                if (openResult == 0)
+                {
+                    isConnected = true;
+                    timer1.Enabled = true;
+
+                    MessageBox.Show("연결되었습니다.");
+                    labelStatus.Text = "현재 상태: PLC 연결 성공";
+                }
+                else
+                {
+                    isConnected = false;
+                    timer1.Enabled = false;
+
+                    MessageBox.Show("연결실패 코드: " + openResult);
+                    labelStatus.Text = "현재 상태: PLC 연결 실패";
+                }
+            }
+            catch (Exception ex)
+            {
+                isConnected = false;
+                timer1.Enabled = false;
+
+                MessageBox.Show("연결 중 오류 발생: " + ex.Message);
+                labelStatus.Text = "현재 상태: PLC 연결 오류";
+            }
+        }
+
+        private void buttonBForward_Click(object sender, EventArgs e)
+        {
+            ManualOutput(Y_B_FORWARD, "B실린더 전진");
+        }
+
+        private void buttonBBack_Click(object sender, EventArgs e)
+        {
+            ManualOutput(Y_B_BACK, "B실린더 후진");
+        }
+
+        private void buttonCForward_Click(object sender, EventArgs e)
+        {
+            ManualOutput(Y_C_FORWARD, "C실린더 전진");
+        }
+
+        private void buttonCBack_Click(object sender, EventArgs e)
+        {
+            ManualOutput(Y_C_BACK, "C실린더 후진");
+        }
+
+        private void buttonLiftAUp_Click(object sender, EventArgs e)
+        {
+            ManualOutput(Y_LIFTA_UP, "LiftA 상승");
+        }
+
+        private void buttonLiftADown_Click(object sender, EventArgs e)
+        {
+            ManualOutput(Y_LIFTA_DOWN, "LiftA 하강");
+        }
+
+        private void buttonLiftBUp_Click(object sender, EventArgs e)
+        {
+            ManualOutput(Y_LIFTB_UP, "LiftB 상승");
+        }
+
+        private void buttonLiftBDown_Click(object sender, EventArgs e)
+        {
+            ManualOutput(Y_LIFTB_DOWN, "LiftB 하강");
+        }
+
+        private void buttonAutoStart_Click(object sender, EventArgs e)
+        {
+            if (!isConnected)
+            {
+                MessageBox.Show("PLC 연결 후 자동운전을 시작하세요.");
+                return;
+            }
+
+            autoMode = true;
+            autoStep = -4;     // 초기정렬 단계부터 시작
+            WriteY(0);
+
+            labelStatus.Text = "자동운전 시작: 초기정렬 시작";
+        }
+
+        private void buttonAutoStop_Click(object sender, EventArgs e)
+        {
+            autoMode = false;
+            autoStep = 0;
+            WriteY(0);
+
+            labelStatus.Text = "현재 상태: 자동운전 정지";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            short sensor;
+
+            if (!ReadSensor(out sensor))
+            {
+                return;
+            }
+
+            UpdateSensorLabel(sensor);
+
+            if (autoMode)
+            {
+                RunAutoSequence(sensor);
+            }
+        }
+       
+        private void RunAutoSequence(short sensor)
+        {
+            switch (autoStep)
+            {
+                case -4:
+                    labelStatus.Text = "초기정렬: B실린더 후진";
+                    WriteY(Y_B_BACK);
+
+                    if (IsOn(sensor, X_B_BACK))
+                    {
+                        WriteY(0);
+                        autoStep = -3;
+                    }
+                    break;
+
+                case -3:
+                    labelStatus.Text = "초기정렬: C실린더 후진";
+                    WriteY(Y_C_BACK);
+
+                    if (IsOn(sensor, X_C_BACK))
+                    {
+                        WriteY(0);
+                        autoStep = -2;
+                    }
+                    break;
+
+                case -2:
+                    labelStatus.Text = "초기정렬: LiftA 상승";
+                    WriteY(Y_LIFTA_UP);
+
+                    if (IsOn(sensor, X_LIFTA_UP))
+                    {
+                        WriteY(0);
+                        autoStep = -1;
+                    }
+                    break;
+
+                case -1:
+                    labelStatus.Text = "초기정렬: LiftB 상승";
+                    WriteY(Y_LIFTB_UP);
+
+                    if (IsOn(sensor, X_LIFTB_UP))
+                    {
+                        WriteY(0);
+                        autoStep = 0;
+                    }
+                    break;
+
+                case 0:
+                    labelStatus.Text = "자동운전 0단계: 센서A 대기";
+
+                    if (IsOn(sensor, X_SENSOR_A))
+                    {
+                        autoStep = 1;
+                    }
+                    break;
+
+                case 1:
+                    labelStatus.Text = "자동운전 1단계: LiftA 상승";
+                    WriteY(Y_LIFTA_UP);
+
+                    if (IsOn(sensor, X_LIFTA_UP))
+                    {
+                        WriteY(0);
+                        autoStep = 2;
+                    }
+                    break;
+
+                case 2:
+                    labelStatus.Text = "자동운전 2단계: B실린더 전진";
+                    WriteY(Y_B_FORWARD);
+
+                    if (IsOn(sensor, X_B_FORWARD))
+                    {
+                        WriteY(0);
+                        autoStep = 3;
+                    }
+                    break;
+
+                case 3:
+                    labelStatus.Text = "자동운전 3단계: B실린더 후진";
+                    WriteY(Y_B_BACK);
+
+                    if (IsOn(sensor, X_B_BACK))
+                    {
+                        WriteY(0);
+                        autoStep = 4;
+                    }
+                    break;
+
+                case 4:
+                    labelStatus.Text = "자동운전 4단계: LiftA 하강";
+                    WriteY(Y_LIFTA_DOWN);
+
+                    if (IsOn(sensor, X_LIFTA_DOWN))
+                    {
+                        WriteY(0);
+                        autoStep = 5;
+                    }
+                    break;
+
+                case 5:
+                    labelStatus.Text = "자동운전 5단계: 센서B 대기";
+
+                    if (IsOn(sensor, X_SENSOR_B))
+                    {
+                        autoStep = 6;
+                    }
+                    break;
+
+                case 6:
+                    labelStatus.Text = "자동운전 6단계: LiftB 하강";
+                    WriteY(Y_LIFTB_DOWN);
+
+                    if (IsOn(sensor, X_LIFTB_DOWN))
+                    {
+                        WriteY(0);
+                        autoStep = 7;
+                    }
+                    break;
+
+                case 7:
+                    labelStatus.Text = "자동운전 7단계: C실린더 전진";
+                    WriteY(Y_C_FORWARD);
+
+                    if (IsOn(sensor, X_C_FORWARD))
+                    {
+                        WriteY(0);
+                        autoStep = 8;
+                    }
+                    break;
+
+                case 8:
+                    labelStatus.Text = "자동운전 8단계: C실린더 후진";
+                    WriteY(Y_C_BACK);
+
+                    if (IsOn(sensor, X_C_BACK))
+                    {
+                        WriteY(0);
+                        autoStep = 9;
+                    }
+                    break;
+
+                case 9:
+                    labelStatus.Text = "자동운전 9단계: LiftB 상승";
+                    WriteY(Y_LIFTB_UP);
+
+                    if (IsOn(sensor, X_LIFTB_UP))
+                    {
+                        WriteY(0);
+                        autoStep = 0;
+                    }
+                    break;
+
+                default:
+                    WriteY(0);
+                    autoMode = false;
+                    autoStep = 0;
+                    labelStatus.Text = "자동운전 오류: 알 수 없는 단계";
+                    break;
+            }
+        }
+    }
+}
+
+
+
+
