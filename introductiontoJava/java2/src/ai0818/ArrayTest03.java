1a25ed28c6ebbb856965e39a09f8da69ff72d325
hyeong jun <dhj00570@gmail.com>
2026-08-18 15:28:37 +0900

    0818 자바 배열 길이 초기화,변환

---
+++ introductiontoJava/java2/src/ai0818/ArrayTest03.java
@@ -0,0 +1,19 @@
+package ai0818;
+
+import java.util.Arrays;
+
+public class ArrayTest03 {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3};
+
+        System.out.println(arr1.length);
+
+        arr1 = Arrays.copyOf(arr1,arr1.length+2);
+
+        System.out.println("추가된 배열의 길이는 :" + arr1.length);
+
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i] + " ");
+        }
+    }
+}
