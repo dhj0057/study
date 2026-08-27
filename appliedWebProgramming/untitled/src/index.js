69001a08ae9df58d02c2dd0a7b3b4b501fcc58b3
hyeong jun <dhj00570@gmail.com>
2026-08-27 11:46:17 +0900

    웹수업 0827

--- appliedWebProgramming/untitled/src/index.js
+++ appliedWebProgramming/untitled/src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
-import TodoApp from "./01/TodoApp";
+//import TodoApp from "./01/TodoApp";
 import reportWebVitals from './reportWebVitals';
+import Library from "./03/Library";
+import './03/Book.css'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TodoApp />
+    <Library />
   </React.StrictMode>
 );
 
